c022681c42069389f4a4c3c4c5e750946cd86317
baleleYtech <balele@ytech-bf.com>
2025-03-22 15:23:28 +0100

    hhhghghg

--- database/migrations/2025_02_27_192619_create_orders_table.php
+++ database/migrations/2025_02_27_192619_create_orders_table.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Models\User;
+use App\Models\Pharmacy;
 use App\Models\Enums\OrderStatus;
 use App\Models\Enums\PayementType;
-use App\Models\Pharmacy;
-use App\Models\User;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -17,13 +17,12 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+           $table->foreignIdFor(User::class)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignIdFor(Pharmacy::class)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
-            $table->string('dateOrder');
             $table->double('priceTotal');
             $table->enum('orderStatus', OrderStatus::values())->default(OrderStatus::default());
             $table->string('adresLivraison');
-            $table->enum('modePayement', PayementType::values())->default(PayementType::default());
+            $table->enum('modePayement', PayementType::values())->default(PayementType::default())->nullable();
             $table->timestamps();
         });
     }
